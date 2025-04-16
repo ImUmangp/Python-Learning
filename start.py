ca03dbfdf830f71adc773649a0b7539b1af8d41a
umang pandey <75161372+ImUmangp@users.noreply.github.com>
2025-04-16 17:54:19 +0530

    commit -added Code in Python

--- start.py
+++ start.py
@@ -1,4 +1,13 @@
 #print("Hello Bhai")
 
-name = input("Enter your name: ")
-print("Hello " + name + ", welcome to the world of Python!")
+#name = input("Enter your name: ")
+#print("Hello " + name + ", welcome to the world of Python!")
+
+a = 10
+b = 20
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is equal to b")
